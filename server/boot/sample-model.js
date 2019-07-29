608dbaa9cb14a25b00806a69a7e9cfabd9826332
hanifabeg <hanifa.beg@gmail.com>
2019-07-29 06:34:33 +0530

    commit

--- server/boot/sample-model.js
+++ server/boot/sample-model.js
@@ -1,125 +1,67 @@
 module.exports = function (app) {
     var User = app.models.Member;
-    var Role = app.models.Role;
-    var RoleMapping = app.models.RoleMapping;
+    var Role = app.models.MemberRole;
+    var RoleMapping = app.models.RoleMemberMapping;
     return new Promise(function (resolve, reject) {
-            User.create([
-                {
-                    name: 'Hanifa Beg'
-                    , empid: '123456'
-                    , username: 'hanifabeg'
-                    , email: 'hanifa.beg@gmail.com'
-                    , password: '123456'
+        User.create([
+            {
+                name: 'Hanifa Beg'
+                , empid: '123456'
+                , username: 'hanifabeg'
+                , email: 'hanifa.beg@gmail.com'
+                , password: '123456'
         },
 
   ], function (err, users) {
-                if (err) reject(err);
-                resolve(users)
-            })
-        }).then(function (users) {
-            console.log('Created users:', users);
-            return new Promise(function (resolve, reject) {
-                //create the superadmin role
-                Role.create({
-                    name: 'SuperAdmin'
-                }, function (err, role) {
-                    if (err) reject(err);
-                    resolve({
-                        "role": role
-                        , "users": users
-                    })
-                })
-            })
-        }).then(function (data) {
-            console.log("data in role", data)
-            console.log('Created users:', data.users);
-            console.log('Created role:', data.role);
-            return new Promise(function (resolve, reject) {
-                data.role.principals.create({
-                    principalType: RoleMapping.USER
-                    , principalId: data.users[0].id
-                }, function (err, principal) {
-                    if (err) throw err;
-                    else {
-                        resolve(data)
-                    }
-                })
-            })
-        }).then(function (data) {
-            console.log('Created principal:', data);
-            return new Promise(function (resolve, reject) {
-                //create the superadmin role
-                Role.create({
-                    name: 'Admin'
-                }, function (err, role) {
-                    if (err) throw err;
-                    else {
-                        resolve(data)
-                    }
-                })
-            })
-        }).then(function (data) {
-            console.log('Created role:', data);
-            var whereFilter = {}
-            whereFilter = {
-                'where': {
-                    'empid': data.empid
-                }
-            }
-            return new Promise(function (resolve, reject) {
-                User.upsertWithWhere(whereFilter, {
-                    "empid": data.empid
-                    , "role": 'SuperAdmin'
-                }, function (err, result) {
-                    if (err) reject(err)
-                    else {
-                        console.log("result", result)
-                        resolve(result)
-                    }
-                })
-            })
-        }).then(function (data) {
-            console.log('Created:', data);
-        }).catch(function (err) {
-            console.log("err", err)
+            if (err) reject(err);
+            resolve(users)
         })
-        /* User.create([
-        {
-            name: 'John'
-            , empid: '1234567'
-            , username: 'John'
-            , email: 'john@doe.com'
-            , password: 'opensesame'
-        },
-
-  ], function (err, users) {
-        if (err) throw err;
+    }).then(function (users) {
         console.log('Created users:', users);
-        //create the superadmin role
-        Role.create({
-            name: 'SuperAdmin'
-        }, function (err, role) {
-            if (err) throw err;
-            console.log('Created role:', role);
-            //make bob an admin
-            role.principals.create({
+        return new Promise(function (resolve, reject) {
+            //create the superadmin role
+            Role.create({
+                name: 'SuperAdmin'
+            }, function (err, role) {
+                if (err) reject(err);
+                resolve({
+                    "role": role
+                    , "users": users
+                })
+            })
+        })
+    }).then(function (data) {
+        console.log("data in role", data)
+        console.log('Created users:', data.users);
+        console.log('Created role:', data.role);
+        return new Promise(function (resolve, reject) {
+            RoleMapping.create({
                 principalType: RoleMapping.USER
-                , principalId: users[0].id
+                , MemberId: data.users[0].id
+                , RoleId: data.role.id
             }, function (err, principal) {
-                if (err) throw err;
-                console.log('Created principal:', principal);
-            });
-        });
-    });*/
-};
-//module.exports = function (app) {
-//    var Role = app.models.Role;
-//    var Member = app.models.Member;
-//    Role.belongsTo(Member, {
-//        foreignKey: 'name'
-//    }, {
-//        properties: function (inst) {
-//            console.log("inst", inst)
-//        }
-//    })
-//}
+                if (err) reject(err);
+                else {
+                    resolve(data)
+                }
+            })
+        })
+    }).then(function (data) {
+        console.log('Created principal:', data);
+        return new Promise(function (resolve, reject) {
+            //create the superadmin role
+            Role.create({
+                name: 'Admin'
+            }, function (err, role) {
+                if (err) reject(err);
+                else {
+                    resolve(data)
+                }
+            })
+        })
+    }).then(function (data) {
+        console.log('Created:', data);
+    }).catch(function (err) {
+        console.log("err", err)
+    })
+}
--- server/models/member.js
+++ server/models/member.js
@@ -1,4 +1,5 @@
 'use strict';
+var ObjectID = require('mongodb');
 module.exports = function (Member) {
     Member.disableRemoteMethod('upsertWithWhere', true);
     Member.disableRemoteMethod('update', true);
@@ -23,30 +24,41 @@ module.exports = function (Member) {
     Member.disableRemoteMethod('upsert', true);
     Member.disableRemoteMethod('deleteById', true);
     Member.disableRemoteMethod("updateAll", true);
-    Member.disableRemoteMethod('create', true);
+    //Member.disableRemoteMethod('create', true);
     Member.disableRemoteMethod('__delete__Member', true);
     Member.makeAdmin = function (email, cb) {
-        var RoleMappingo = Member.app.models.RoleMapping;
+        var RoleMappingo = Member.app.models.RoleMemberMapping;
         var mainObject = {};
         Member.findOne({
             where: {
                 email: email
             }
         }, function (err, data) {
+            if (err) return cb(null, {
+                title: 'Some error occred'
+                , content: err
+            });
             console.log("data MakeAdmin", data)
             mainObject.userid = data.id;
-            Member.app.models.Role.findOne({
+            Member.app.models.MemberRole.findOne({
                 where: {
                     name: "Admin"
                 }
             }, function (err, data) {
+                if (err) return cb(null, {
+                    title: 'Some error occred'
+                    , content: err
+                });
                 console.log("pppp", data)
                 RoleMappingo.create({
                     principalType: RoleMappingo.ROLE
-                    , principalId: mainObject.userid
-                    , roleId: data.id
+                    , MemberId: mainObject.userid
+                    , RoleId: data.id
                 }, function (err, principal) {
-                    if (err) throw err;
+                    if (err) return cb(null, {
+                        title: 'Some error occred'
+                        , content: err
+                    });
                     else {
                         console.log("principal", principal)
                         return cb(null, {
@@ -57,95 +69,159 @@ module.exports = function (Member) {
             })
         })
     }
-    Member.login = function (credentials, include, cb) {
-        var User = Member.app.models.User;
-        var RoleMapping = Member.app.models.RoleMapping;
-        var mainObject = {};
-        // {include: 'posts'}
-        var Role = Member.app.models.Role;
-        User.login(credentials, 'user', function (err, token) {
-            if (err) {
-                console.log("errrrrrr", err)
-                cb(null, {
+    Member.login = function (credentials, include, fn) {
+            var mainObject = {}
+            this.findOne({
+                where: {
+                    email: credentials.email
+                }
+            }, function (err, user) {
+                if (err || (!user)) return fn(null, {
                     title: 'Login failed'
                     , content: err
                 });
-                return;
-            }
-            console.log('token+++++++', token)
-            mainObject.id = token.id;
-            User.findOne({
-                where: {
-                    _id: token.userId
-                }
-            }, function (err, data) {
-                mainObject.email = data.email
-                mainObject.username = data.username
-                mainObject.empid = data.empid
-                mainObject.password = data.password
-                Member.findOne({
+                console.log("user", user)
+                mainObject.user = user
+                console.log(mainObject.user.id)
+                Member.find({
                     where: {
-                        email: mainObject.email
+                        email: mainObject.user.email
                     }
-                }, function (err, data) {
-                    if (err) console.log("err", err)
-                    else {
-                        console.log("data", data);
-                        if (!data) {
-                            Member.create([
-                                {
-                                    _id: {
-                                        "$toObjectId": mainObject.id
-                                    }
-                                    , name: mainObject.username
-                                    , empid: mainObject.empid
-                                    , username: mainObject.username
-                                    , email: mainObject.email
-                                    , password: mainObject.password
-                            },
-
-                      ], function (err, user) {
-                                if (err) console.log(err);
-                                console.log(user)
-                                if (!user[0].role) {
-                                    role = "Non admin";
-                                }
-                                return cb(null, {
-                                    username: user[0].username
-                                    , id: user[0].id
-                                    , email: user[0].email
-                                    , accessToken: mainObject.id
+                    , include: 'roles'
+                }, function (err, roles) {
+                    if (err) return fn(null, {
+                        title: 'Some error occred'
+                        , content: err
+                    });
+                    console.log("roles", roles)
+                    roles.forEach(function (ele, index) {
+                        ele.roles.find({}, function (err, data) {
+                            if (err) return fn(500, {
+                                title: 'Some error occred'
+                                , content: err
+                            });
+                            console.log(data.length)
+                            user.createAccessToken(86400, function (err, token) {
+                                if (err) return fn(null, {
+                                    title: 'Some error occred'
+                                    , content: err
                                 });
-                            })
-                        }
-                        else {
-                            console.log(data)
-                            var MainData = data
-                            RoleMapping.findOne({
-                                where: {
-                                    principalId: data.id
+                                console.log("token", token)
+                                if (err) return fn(err);
+                                token.__data.user = user;
+                                if (data.length == 0) {
+                                    token.__data.user.role = "Non Admin"
                                 }
-                            }, function (err, data) {
-                                console.log("RoleMapping", data)
-                                Role.findOne({
-                                    where: {
-                                        _id: data.roleId
-                                    }
-                                }, function (err, data) {
-                                    return cb(null, {
-                                        username: MainData.username
-                                        , role: data.name
-                                        , email: MainData.email
-                                        , accessToken: mainObject.id
-                                    });
-                                })
-                            })
-                        }
-                    }
+                                else {
+                                    token.__data.user.role = data[0].name
+                                }
+                                fn(err, token);
+                            });
+                        })
+                    })
                 })
             })
-        });
-    }
+        }
+        //    Member.login = function (credentials, include, cb) {
+        //        console.log(include)
+        //        console.log(cb)
+        //        var User = Member.app.models.User;
+        //        var RoleMapping = Member.app.models.RoleMapping;
+        //        var AccessToken = Member.app.models.AccessToken;
+        //        var mainObject = {};
+        //        // {include: 'posts'}
+        //        var Role = Member.app.models.Role;
+        //        User.generateVerificationToken(credentials, function (err, token) {
+        //            console.log('token+++++++', token)
+        //        })
+        //        console.log("credentials", credentials)
+        //        createAccessToken()
+        //        AccessToken.createAccessTokenId(credentials, function (err, token) {
+        //                console.log('token+++++++', token)
+        //            })
+        //        User.login(credentials, 'user', function (err, token) {
+        //            if (err) {
+        //                console.log("errrrrrr", err)
+        //                cb(null, {
+        //                    title: 'Login failed'
+        //                    , content: err
+        //                });
+        //                return;
+        //            }
+        //            console.log('token+++++++', token)
+        //            mainObject.id = token.id;
+        //            User.findOne({
+        //                where: {
+        //                    _id: token.userId
+        //                }
+        //            }, function (err, data) {
+        //                mainObject.email = data.email
+        //                mainObject.username = data.username
+        //                mainObject.empid = data.empid
+        //                mainObject.password = data.password
+        //                Member.findOne({
+        //                    where: {
+        //                        email: mainObject.email
+        //                    }
+        //                }, function (err, data) {
+        //                    if (err) console.log("err", err)
+        //                    else {
+        //                        console.log("data", data);
+        //                        if (!data) {
+        //                            Member.create([
+        //                                {
+        //                                    _id: {
+        //                                        "$toObjectId": mainObject.id
+        //                                    }
+        //                                    , name: mainObject.username
+        //                                    , empid: mainObject.empid
+        //                                    , username: mainObject.username
+        //                                    , email: mainObject.email
+        //                                    , password: mainObject.password
+        //                            },
+        //
+        //                      ], function (err, user) {
+        //                                if (err) console.log(err);
+        //                                console.log(user)
+        //                                if (!user[0].role) {
+        //                                    role = "Non admin";
+        //                                }
+        //                                return cb(null, {
+        //                                    username: user[0].username
+        //                                    , id: user[0].id
+        //                                    , email: user[0].email
+        //                                    , accessToken: mainObject.id
+        //                                });
+        //                            })
+        //                        }
+        //                        else {
+        //                            console.log(data)
+        //                            var MainData = data
+        //                            RoleMapping.findOne({
+        //                                where: {
+        //                                    principalId: data.id
+        //                                }
+        //                            }, function (err, data) {
+        //                                console.log("RoleMapping", data)
+        //                                Role.findOne({
+        //                                    where: {
+        //                                        _id: data.roleId
+        //                                    }
+        //                                }, function (err, data) {
+        //                                    return cb(null, {
+        //                                        username: MainData.username
+        //                                        , role: data.name
+        //                                        , email: MainData.email
+        //                                        , accessToken: mainObject.id
+        //                                    });
+        //                                })
+        //                            })
+        //                        }
+        //                    }
+        //                })
+        //            })
+        //        });
+        // }
     Member.remoteMethod('makeAdmin', {
         accepts: {
             arg: 'email'
